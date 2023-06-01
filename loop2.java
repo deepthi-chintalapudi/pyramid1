dc099de6f429ce0af036965603d54e81a8be3719
deepthi-chintalapudi <125786931+deepthi-chintalapudi@users.noreply.github.com>
2023-06-01 22:43:04 +0530

    Add files via upload

---
+++ loop2.java
@@ -0,0 +1,11 @@
+public class loop2 {
+    public static void main(String[] args) {
+        int i,j;
+        for(i=1;i<=5;i++){
+            for(j=1;j<=i-1;j++){
+                System.out.print(j+" ");
+            }
+            System.out.println( );
+        }
+    }
+}
